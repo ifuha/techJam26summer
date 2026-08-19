51707be3237ac96af28717ae4440d459d0772ec7
ifuji <ifujihaku1216@gmail.com>
2026-08-19 13:00:42 +0900

    DB修正

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f40d8bed27171a7b4b32732a85b74d5c45101a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ec9593d17703fa0962dfca970d169062ec96d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
