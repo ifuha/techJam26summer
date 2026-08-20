664d0905c9e02cd77261de5d20de20d4aaafee9a
ifuji <ifujihaku1216@gmail.com>
2026-08-20 11:03:46 +0900

    feat: アイコンコンポーネントとグループSVGを追加

---
+++ web/src/components/icons/icon.tsx
@@ -0,0 +1,23 @@
+import { Group } from "./svg/group";
+
+const icons = {
+  group: Group,
+} as const;
+export type IconProps = {
+  size: number;
+  name: keyof typeof icons;
+};
+
+export function Icon({ size, name }: IconProps) {
+  const IconComponent = icons[name];
+  return (
+    <div
+      style={{
+        width: size,
+        height: size,
+      }}
+    >
+      <IconComponent />
+    </div>
+  );
+}
